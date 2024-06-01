cb3b222f9fd7dfaf646fef2e074da13a82a3b1ea
GranRoberto <robertocariolo94@gmail.com>
2024-05-31 22:47:02 -0400

    gg

--- Juego/juego.js
+++ Juego/juego.js
@@ -1,12 +1,8 @@
 var jugador1= prompt("Nombre del Jugador 1:")
 var jugador2= prompt("Nombre del Jugador 2:")
-
 var opcion1 = prompt((jugador1) + ", elige una opción: piedra, papel o tijera:");
 var opcion2 = prompt((jugador2) + ", elige una opción: piedra, papel o tijera:");
-
 var opciones = ["piedra", "papel", "tijera"];
-var resultado = [alert];
-console.log(resultado);
 
 if (opcion1 === opcion2) {
   alert("Empate");
--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,7 +1,6 @@
 var numero1= prompt("Indique el primer número de la operación")
 var operacion= prompt("Indique el tipo de operación que desea realizar: + Suma, - Resta, * Multiplicación, / División, ** Exponenciación")
 var numero2= prompt("Indique el segundo número de la operación")
-console.log= alert;
 
 if (operacion === "+") {
   alert(parseFloat(numero1) + parseFloat(numero2));

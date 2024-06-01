70cea381f9defa008177f927661ffcd8d88a87af
GranRoberto <robertocariolo94@gmail.com>
2024-05-31 22:30:45 -0400

    a

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -3,14 +3,16 @@ var operacion= prompt("Indique el tipo de operación que desea realizar: + Suma,
 var numero2= prompt("Indique el segundo número de la operación")
 console.log= alert;
 
-if (operacion === ("-")) {
-  alert(numero1 - numero2);
-} else if (operacion === ("+")) {
-  alert(numero1 + numero2);
-} else if (operacion === ("/")) {
-  alert(numero1 / numero2);
-} else if (operacion === ("*")) {
-  alert(numero1 * numero2);
-} else if (operacion === ("**")) {
-  alert(numero1 ** numero2);
+if (operacion === "+") {
+  alert(parseFloat(numero1) + parseFloat(numero2));
+} else if (operacion === "-") {
+  alert(parseFloat(numero1) - parseFloat(numero2));
+} else if (operacion === "/") {
+  alert(parseFloat(numero1) / parseFloat(numero2));
+} else if (operacion === "*") {
+  alert(parseFloat(numero1) * parseFloat(numero2));
+} else if (operacion === "**") {
+  alert(parseFloat(numero1) ** parseFloat(numero2));
+} else {
+  alert("Operación no válida");
 }

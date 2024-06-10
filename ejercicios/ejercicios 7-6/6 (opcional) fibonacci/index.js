531064e25a59fdaaa53eb7f9a93fa8168055cdb3
GranRoberto <robertocariolo94@gmail.com>
2024-06-10 18:24:36 -0400

    a

--- ejercicios/ejercicios 7-6/6 (opcional) fibonacci/index.js
+++ ejercicios/ejercicios 7-6/6 (opcional) fibonacci/index.js
@@ -1,22 +1,20 @@
 function printFibonacciSequence(n) {
 
-  let n = prompt("Indica cuantos valores de la escala de fibonacci deseas ver:");
-
   if (n <= 0) {
     throw new alert('Error: debe ser un numero positivo');
   }
 
   let anterior = 0;
-  let uso = 1;
+  let nuevo = 1;
 
   console.log(0);
 
   for (let i = 1; i < n; i++) {
-    const siguiente = anterior + uso;
+    const siguiente = anterior + nuevo;
     console.log(siguiente);
-    anterior = uso;
-    uso = siguiente;
+    anterior = nuevo;
+    nuevo = siguiente;
   }
 }
 
-printFibonacciSequence();
+printFibonacciSequence(8);

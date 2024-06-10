6414958c6d36a182ff564fc4f6613c8896543304
GranRoberto <robertocariolo94@gmail.com>
2024-06-10 18:12:17 -0400

    a

--- ejercicios/ejercicios 7-6/5 hipotenusa/index.js
+++ ejercicios/ejercicios 7-6/5 hipotenusa/index.js
@@ -2,7 +2,7 @@ function hipotenusa (base, altura){
   var base = prompt("Indique el valor de la base del triangulo:");
   var altura = prompt("Indique el valor de la altura del triangulo:");
 
-  return alert('La hipotenusa del triangulo es: ' + (Math.sqrt((base * base) + (altura * altura))));
+  return alert('La hipotenusa del triangulo es: ' + (Math.sqrt((base **2) + (altura **2))));
 }
 
 hipotenusa();
